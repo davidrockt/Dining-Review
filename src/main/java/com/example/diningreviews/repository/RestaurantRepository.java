69b6838a50843b0b1861994b141ba3f00b2830c0
David W <david.waldmann@mni.thm.de>
2021-09-01 23:50:52 +0200

    Alles löschen was ich selbst schreiben will

--- src/main/java/com/example/diningreviews/repository/RestaurantRepository.java
+++ src/main/java/com/example/diningreviews/repository/RestaurantRepository.java
@@ -3,12 +3,5 @@ package com.example.diningreviews.repository;
 import com.example.diningreviews.model.Restaurant;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
-    Optional<Restaurant> findRestaurantsByNameAndZipCode(String name, String zipCode);
-    List<Restaurant> findRestaurantsByZipCodeAndPeanutScoreNotNullOrderByPeanutScore(String zipcode);
-    List<Restaurant> findRestaurantsByZipCodeAndDairyScoreNotNullOrderByDairyScore(String zipcode);
-    List<Restaurant> findRestaurantsByZipCodeAndEggScoreNotNullOrderByEggScore(String zipcode);
 }
